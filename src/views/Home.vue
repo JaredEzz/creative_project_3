14baf00363384b47dedc30fda3e88dea87a8378f
Jared Hasson <jhasson@simplifile.com>
2020-03-19 10:04:18 -0600

    getting started

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Jared Creative Project 3"/>
   </div>
 </template>
 
