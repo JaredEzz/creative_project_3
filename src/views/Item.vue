e8a2a5f55dad881009f07f5cd7d6e8fa526a4482
Jared Hasson <jhasson@simplifile.com>
2020-03-19 13:26:55 -0600

    add missing files

---
+++ src/views/Item.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="item">
+    <h2>{{this.itemData.name}}</h2>
+    <img v-bind:src="this.itemData.icon_large"/>
+    <h4>Type: {{itemData.type}}</h4>
+    <h5>{{itemData.description}}</h5>
+    <br>
+    <p>Current Price — {{this.itemData.current.price}} gp</p>
+    <p>Members Item? {{this.itemData.members ? "Yes" : "No"}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    computed: {
+      itemData() {
+        return this.$root.$data.itemData.items[0];
+      },
+    },
+  }
+</script>
